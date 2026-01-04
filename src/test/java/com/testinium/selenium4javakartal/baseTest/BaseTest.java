54191e5ba897512fe87c2f227ac51c8368ff4447
Furkan Kartal <Furkan.kartal@testinium.com>
2026-01-04 14:04:52 +0300

    deneme

--- src/test/java/com/testinium/selenium4javakartal/baseTest/BaseTest.java
+++ src/test/java/com/testinium/selenium4javakartal/baseTest/BaseTest.java
@@ -3,12 +3,14 @@ package com.testinium.selenium4javakartal.baseTest;
 import com.testinium.driver.TestiniumSeleniumDriver;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseTest {
 
     protected RemoteWebDriver driver;
@@ -16,7 +18,6 @@ public abstract class BaseTest {
     @BeforeAll
     void setUp() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
-
 
         String remoteUrl = System.getenv().getOrDefault(
                 "SELENIUM_REMOTE_URL",
