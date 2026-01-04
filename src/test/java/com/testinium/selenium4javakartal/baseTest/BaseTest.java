f5a103e02486dc936cb23340892d12498030c998
Furkan Kartal <Furkan.kartal@testinium.com>
2026-01-04 14:31:05 +0300

    deneme

--- src/test/java/com/testinium/selenium4javakartal/baseTest/BaseTest.java
+++ src/test/java/com/testinium/selenium4javakartal/baseTest/BaseTest.java
@@ -1,9 +1,12 @@
 package com.testinium.selenium4javakartal.baseTest;
 
 import com.testinium.driver.TestiniumSeleniumDriver;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -11,12 +14,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public abstract class BaseTest {
+public abstract class BaseTest implements BeforeAllCallback, AfterAllCallback {
 
     protected RemoteWebDriver driver;
 
-    @BeforeAll
-    void setUp() throws MalformedURLException {
+    @Override
+    public void beforeAll(ExtensionContext context) throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
 
         String remoteUrl = System.getenv().getOrDefault(
@@ -27,8 +30,8 @@ public abstract class BaseTest {
         driver = new TestiniumSeleniumDriver(new URL(remoteUrl), options);
     }
 
-    @AfterAll
-    void tearDown() {
+    @Override
+    public void afterAll(ExtensionContext context) {
         if (driver != null) {
             driver.quit();
         }
