c39c8d86dcf94d80f24e10c21add494e96a43b36
Furkan Kartal <Furkan.kartal@testinium.com>
2026-01-04 13:35:52 +0300

    deneme

---
+++ src/test/java/com/testinium/selenium4javakartal/baseTest/BaseTest.java
@@ -0,0 +1,35 @@
+package com.testinium.selenium4javakartal.baseTest;
+
+import com.testinium.driver.TestiniumSeleniumDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public abstract class BaseTest {
+
+    protected RemoteWebDriver driver;
+
+    @BeforeEach
+    void setUp() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();
+
+
+        String remoteUrl = System.getenv().getOrDefault(
+                "SELENIUM_REMOTE_URL",
+                "http://host.docker.internal:4444/wd/hub"
+        );
+
+        driver = new TestiniumSeleniumDriver(new URL(remoteUrl), options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
